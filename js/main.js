200aaa1b785f239749c9244601f50f9b9d043299
melowny <dani.hediger@gmail.com>
2023-12-24 15:32:12 +0100

    API Calls reduced

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
+let btcPrice = null; // Global variable to store BTC price
 
+// Function to fetch BTC price from Bybit
 async function fetchBTCPriceFromBybit() {
     try {
         const url = 'https://api.bybit.com/v2/public/tickers?symbol=BTCUSD';
         const response = await fetch(url);
         const data = await response.json();
         const btcTicker = data.result.find(ticker => ticker.symbol === 'BTCUSD');
-        return btcTicker ? btcTicker.last_price : null;
+        btcPrice = btcTicker ? btcTicker.last_price : null;
     } catch (error) {
         console.error('Error fetching BTC price from Bybit:', error);
-        return null;
+        btcPrice = null;
     }
 }
 
 let timerInterval;
+
+// Function to start a countdown timer
 function startTimer() {
     let seconds = 15; // Start from 15 seconds
     timerInterval = setInterval(function() {
@@ -26,10 +30,8 @@ function startTimer() {
     }, 1000);
 }
 
-
-
+// Function to update the BTC price display
 async function updateBTCPriceDisplay() {
-    const btcPrice = await fetchBTCPriceFromBybit();
     if (btcPrice) {
         document.getElementById('btcPrice').innerText = btcPrice;
         calculateTargets();
@@ -40,13 +42,17 @@ async function updateBTCPriceDisplay() {
     }
 }
 
+// Function to periodically update BTC price
 function updateBTCPricePeriodically() {
-    const updateInterval = 15000;
-    setInterval(updateBTCPriceDisplay, updateInterval);
+    const updateInterval = 15000; // 15 seconds
+    setInterval(async () => {
+        await fetchBTCPriceFromBybit();
+        updateBTCPriceDisplay();
+    }, updateInterval);
 }
 
+// Function to calculate targets
 async function calculateTargets() {
-    const btcPrice = await fetchBTCPriceFromBybit();
     if (!btcPrice) {
         document.getElementById('liqPrice').innerText = 'Unavailable';
         document.getElementById('priceDifference').innerText = 'Unavailable';
@@ -59,52 +65,45 @@ async function calculateTargets() {
     const tableBody = document.getElementById('priceTableBody');
     tableBody.innerHTML = '';
 
-    // Calculate liquidation price
+    // Calculate and update liquidation price and price difference
     const distanceToLiquidation = 100 / leverage;
     const percentageChange = distanceToLiquidation / 100;
     const liquidationPrice = btcPrice * (1 - percentageChange);
-
-    document.getElementById('liqPrice').innerText = '' + liquidationPrice.toFixed(2);
-
-    // Calculate and update the difference between BTC price and liquidation price
+    document.getElementById('liqPrice').innerText = liquidationPrice.toFixed(2);
     const priceDifference = btcPrice - liquidationPrice;
-    document.getElementById('priceDifference').innerText = '' + priceDifference.toFixed(2);
+    document.getElementById('priceDifference').innerText = priceDifference.toFixed(2);
 
+    // Populate the table with calculated values
     percentageChanges.forEach(change => {
         const adjustedChange = change / 100;
         const ammIndicator = (adjustedChange / leverage * 100).toFixed(2);
         const targetPriceIncrease = btcPrice * (1 + adjustedChange / leverage);
         const targetPriceDecrease = btcPrice * (1 - adjustedChange / leverage);
-
-        // Gain calculation based on position size and percentage change
         const gain = positionSize * adjustedChange;
-
         const usdDifference = Math.abs(targetPriceIncrease - btcPrice).toFixed(2);
 
         const rowHTML = `
-                <tr>
-                    <td>${change}%</td>
-                    <td class="amm-indicator">${ammIndicator}%</td>
-                    <td><span class="increase">$${targetPriceIncrease.toFixed(2)}</span> / <span class="decrease">$${targetPriceDecrease.toFixed(2)}</span></td>
-                    <td>$${usdDifference}</td>
-                    <td>$${gain.toFixed(2)}</td>
-                </tr>`;
+            <tr>
+                <td>${change}%</td>
+                <td class="amm-indicator">${ammIndicator}%</td>
+                <td><span class="increase">$${targetPriceIncrease.toFixed(2)}</span> / <span class="decrease">$${targetPriceDecrease.toFixed(2)}</span></td>
+                <td>$${usdDifference}</td>
+                <td>$${gain.toFixed(2)}</td>
+            </tr>`;
         tableBody.innerHTML += rowHTML;
     });
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
-    await updateBTCPriceDisplay();
-    updateBTCPricePeriodically();
-});
+// Function to update leverage value display
 function updateLeverageValue() {
     const leverageValue = document.getElementById('leverageSlider').value;
     document.getElementById('leverageValue').innerText = leverageValue + 'x';
 }
 
-// Call updateLeverageValue on page load to set initial value
+// Event listener for DOMContentLoaded
 document.addEventListener('DOMContentLoaded', async () => {
     updateLeverageValue();
-    await updateBTCPriceDisplay();
+    await fetchBTCPriceFromBybit();
+    updateBTCPriceDisplay();
     updateBTCPricePeriodically();
 });
